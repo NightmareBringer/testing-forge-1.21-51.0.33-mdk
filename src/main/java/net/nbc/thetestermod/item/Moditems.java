011832ef9d273dbd472a3d97df4acea932193ac9
NightmareBringa <xcountrytim@gmail.com>
2024-11-01 18:52:42 -0700

    TestItems

---
+++ src/main/java/net/nbc/thetestermod/item/Moditems.java
@@ -0,0 +1,25 @@
+package net.nbc.thetestermod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.nbc.thetestermod.TesterMod;
+
+public class Moditems
+{
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TesterMod.MOD_ID);
+
+    public static final RegistryObject<Item> NIGHTMARITE = ITEMS.register("nightmarite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PURE_NIGHTMARITE = ITEMS.register("pure_nightmarite",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
