3a64a14792357f271c315954d2ca693dcbad10bb
NightmareBringa <xcountrytim@gmail.com>
2024-11-01 17:09:48 -0700

    init2

--- src/main/java/net/nbc/thetestermod/TesterMod.java
+++ src/main/java/net/nbc/thetestermod/TesterMod.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-// comment of comment
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TesterMod.MOD_ID)
 public class TesterMod
