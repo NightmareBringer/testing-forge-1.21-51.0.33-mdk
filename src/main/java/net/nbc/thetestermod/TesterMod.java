c27db160932800f0273e5899c34ae35402e1dfe6
NightmareBringer <xcountrytim@gmail.com>
2025-04-07 21:48:21 -0700

    NeoForge Port Update (NO FIXES)

--- src/main/java/net/nbc/thetestermod/TesterMod.java
+++ src/main/java/net/nbc/thetestermod/TesterMod.java
@@ -5,7 +5,7 @@ import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.ComposterBlock;
-import net.minecraftforge.api.distmarker.Dist;
+import net.neoforged.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.common.MinecraftForge;
